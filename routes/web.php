11d22a38250b3456089f6218a7265ed694f98069
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2019-04-09 12:02:11 +0600

    git ignore added and test controller created

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// calling complain type list routes
+Route::get('admin/complain-type', 'scmsp\ComplainTypeController@index')->name('admin.complain-type-list');
+
+// calling complain type create routes
+Route::get('admin/create-complain-type', 'scmsp\ComplainTypeController@create')->name('admin.complain-type-create');
+
+// calling complain type edit routes
+Route::get('admin/edit-complain-type', 'scmsp\ComplainTypeController@edit')->name('admin.complain-type-edit');
+
+// calling complain type store routes
+Route::get('admin/store-complain-type', 'scmsp\ComplainTypeController@store')->name('admin.complain-type-store');
+
+// calling complain type update routes
+Route::get('admin/update-complain-type', 'scmsp\ComplainTypeController@update')->name('admin.complain-type-update');
