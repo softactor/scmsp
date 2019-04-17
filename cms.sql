9063a65c906ec49e8387274f5b46f73d9f355398
softactor <tanveerqureshee1@gmail.com>
2019-04-17 17:38:53 +0600

    change git ignor file and sql file

--- cms.sql
+++ cms.sql
@@ -1,33 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 15, 2019 at 06:08 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.1.27
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `cms`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `complain_asigned`
---
-
 CREATE TABLE `complain_asigned` (
   `id` int(11) NOT NULL,
   `complian_id` int(11) NOT NULL,
@@ -260,7 +230,3 @@ ALTER TABLE `migrations`
 ALTER TABLE `users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
