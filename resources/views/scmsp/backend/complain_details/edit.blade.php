5a639517dc3d28c9ca7db281fdfd7e06e7d42a34
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2019-04-18 16:09:33 +0600

    view files update

---
+++ resources/views/scmsp/backend/complain_details/edit.blade.php
@@ -0,0 +1,32 @@
+@extends('scmsp.backend.layout.app')
+@section('title', 'Edit Complain Details')
+@section('content')
+<div class="container-fluid">
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="#">Edit Complain Details</a>
+        </li>
+        <li class="breadcrumb-item active">Overview</li>
+    </ol>
+    <div class='row'>
+        <div class='col col-md-12'>
+            <h2>Edit Complain Details</h2>
+            <form action="/action_page.php">
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Password:</label>
+                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+                </div>
+                <div class="checkbox">
+                    <label><input type="checkbox" name="remember"> Remember me</label>
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
