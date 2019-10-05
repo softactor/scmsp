a9343493f42a1e1092db14772777143f7e557660
softactor <tanveerqureshee1@gmail.com>
2019-10-05 11:58:30 +0600

    va;idation check

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,11 +31,20 @@ class HomeController extends Controller
         $emails['to'] = 'tanveerqureshee1@gmail.com';
         $emails['from_address'] = 'mail.saifpowergroup.com';
         $emails['from_name'] = 'Saif Customer Care';
+        print '<pre>';
+        print_r($emails);
+        print '</pre>';
+        
         $mail = Mail::send('Test', ['title' => 'Title', 'content' => 'Content'], function ($message) use ($emails) {
                     $message->from($emails['from_address'], $emails['from_name']);
                     $message->to($emails['to']);
                     $message->subject("Complain Test Mail");
                 });
+                print '<pre>';
+                print_r($mail);
+                print '</pre>';
+                exit;
+                
     }
 
 }
