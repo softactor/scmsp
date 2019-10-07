efb99bda6a22ea5dcd8a8c0e7e3af229226a7b4f
softactor <tanveerqureshee1@gmail.com>
2019-10-07 12:43:03 +0600

    user create and update validation set

--- app/Http/Controllers/scmsp/ReportsController.php
+++ app/Http/Controllers/scmsp/ReportsController.php
@@ -14,27 +14,7 @@ use Illuminate\Support\Facades\DB;
 class ReportsController extends Controller {
     public function index(Request $request){
         $role   =   getRoleNameByUserId(Auth::user()->id);
-            if($role== 'Admin' || $role=='Agent'){
-                if(isset($complain_status) && !empty($complain_status)){
-                    $list   = ComplainDetails::where('complain_status',$complain_status)->orderBy('created_at', 'DESC')->get();
-                }else{
-                    $list   = ComplainDetails::orderBy('created_at', 'DESC')->get();
-                }
-            }else if($role  ==   'Area Manager'){
-                if(isset($complain_status) && !empty($complain_status)){
-                    $list   = get_complain_details_by_area_manager(Auth::user()->id, $complain_status);
-                }else{
-                    $list   = get_complain_details_by_area_manager(Auth::user()->id);
-                }
-            }else{
-                if(isset($complain_status) && !empty($complain_status)){
-                    $list   = ComplainDetails::where('complain_status',$complain_status)->where('assign_to',Auth::user()->id)->orderBy('created_at', 'DESC')->get();
-                }else{
-                    $list   = ComplainDetails::where('assign_to',Auth::user()->id)->orderBy('created_at', 'DESC')->get();
-                }
-            }
-            /* selected menue data */
-            $activeMenuClass    =   'complain-details';   
-            return View('scmsp.backend.complain_details.list', compact('list','activeMenuClass'));
+        $activeMenuClass    =   'report-list';   
+        return View('scmsp.backend.reports.index', compact('list','activeMenuClass'));
     }
 }
