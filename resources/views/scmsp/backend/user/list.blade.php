34a92b278c8b7291db1acf66564ce9ca68b046e2
softactor <tanveerqureshee1@gmail.com>
2019-11-21 09:59:21 +0600

    added division in user list

--- resources/views/scmsp/backend/user/list.blade.php
+++ resources/views/scmsp/backend/user/list.blade.php
@@ -16,6 +16,7 @@
             <table class="table table-bordered list-table-custom-style" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
+                            <th>Division</th>
                             <th>Name</th>
                             <th>Email</th>
                             <th>Role</th>
@@ -23,15 +24,6 @@
                             <th>Action</th>
                         </tr>
                     </thead>
-                    <tfoot>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Role</th>
-                            <th>Mobile</th>
-                            <th>Action</th>
-                        </tr>
-                    </tfoot>
                     <tbody>
                         <?php
                             $deleteUrl  =   url('admin/user-delete');
@@ -39,6 +31,16 @@
                                 foreach($list as $data){
                         ?>
                         <tr id='delete_row_id_{{$data->id}}'>
+                            <td>
+                                <?php
+                                    if(isset($data->division_id) && !empty($data->division_id)){
+                                        $divname    =   get_data_name_by_id('departments',$data->division_id);
+                                        if(isset($divname) && !empty($divname)){
+                                            echo $divname->name;
+                                        }
+                                    }
+                                ?>
+                            </td>
                             <td><a href="{{ url('admin/user-edit/'.$data->id) }}" title="Click here to edit">{{ $data->name }}</a></td>
                             <td>{{ $data->email }}</td>
                             <td><?php echo (isset($data->role_id) && !empty($data->role_id) ? get_data_name_by_id('roles',$data->role_id)->name : 'Role unassigned!') ?></td>
